94dd7a22a1a184d0a3c8a764bb802d78eee883b5
andreyrobot <kr4t0s.vieir4@gmail.com>
2025-09-16 09:52:20 -0300

    Delete ARQUIVO.py

--- ARQUIVO.py
+++
@@ -1,6 +0,0 @@
-nome = input("Digite seu nome cidadão😁: ")
-email = input(f"Poderia me Afirmar Seu {nome} e-mail: ")
-
-arquivo = open ("pessoa.txt" , "a")
-arquivo.write(nome + " | " + email + "/n")
-arquivo.close
